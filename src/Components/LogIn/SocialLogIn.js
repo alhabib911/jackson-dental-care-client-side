a5a88bf3aa581ddf1f76b906dd7e5f3a5d0e2b97
alhabib911 <abdullahalhabib100@gmail.com>
2022-04-18 06:00:37 +0600

    google authentication done

--- src/Components/LogIn/SocialLogIn.js
+++ src/Components/LogIn/SocialLogIn.js
@@ -1,11 +1,24 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 
-
+const auth = getAuth();
 const SocialLogIn = () => {
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn =() =>{
+        signInWithPopup(auth, provider)
+        .then(result =>{
+         const user = result.user;
+         console.log(user);    
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
     return (
         <div>
-            <button><FcGoogle></FcGoogle><span>Google Log In</span></button>
+            <button onClick={handleGoogleSignIn}><FcGoogle></FcGoogle><span>Google Log In</span></button>
         </div>
     );
 };
